3874100d93361768ba7ef2a960aa11a3893c1e45
Chen Yu Ling <yo0.guitar.it@gmail.com>
2024-06-06 17:31:38 +0800

    feat: finish module css style

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,25 +1,39 @@
 import React from "react";
 import { Link } from "gatsby";
-
+import {
+  container,
+  heading,
+  navLinks,
+  navLinksItem,
+  navLinksText,
+} from "./layout.module.css";
 
 const Layout = (props) => {
   const pageTitle = props.pageTitle;
   const children = props.children;
 
   return (
-    <div>
+    <div className={container}>
       <nav>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/about'>About</Link></li>
+        <ul className={navLinks}>
+          <li className={navLinksItem}>
+            <Link to="/" className={navLinksText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinksItem}>
+            <Link to="/about" className={navLinksText}>
+              About
+            </Link>
+          </li>
         </ul>
       </nav>
       <main>
-        <h1>{pageTitle}</h1>
+        <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
     </div>
-  )
-}
+  );
+};
 
 export default Layout;
