48d909822fde1dceb433aa62a3045167f44abc1e
Chen Yu Ling <yo0.guitar.it@gmail.com>
2024-06-07 00:16:08 +0800

    feat:finish add plugin for photo

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const AboutPage = () => {
   return (
@@ -7,10 +8,11 @@ const AboutPage = () => {
       <p>
         Hi there! I'm the proud creater of this site, which I build with Gatsby.
       </p>
+      <StaticImage alt="test image" src="../images/test.jpg" />
     </Layout>
   );
 };
 
-export const Head = () => <title>About Me</title>
+export const Head = () => <title>About Me</title>;
 
 export default AboutPage;
