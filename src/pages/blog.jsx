8c71f7243fcdc5eea6bf356a156dcc0746d5d0dd
Mac <yo0.guitar.it@gmail.com>
2024-06-07 15:30:30 +0800

    feat: add list for blog

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import { graphql } from "gatsby";
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My blog Posts">
-      <p>My Cool posts will go in here</p>
+      <ul>
+        {data.allFile.edges.map((element) => (
+          <li key={element.node.name}>{element.node.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 };
 
 export const Head = () => <Seo title="My Blog Posts" />;
+
+export const query = graphql`
+  query {
+    allFile(filter: { extension: { in: ["md", "mdx"] } }) {
+      edges {
+        node {
+          name
+        }
+      }
+    }
+  }
+`;
 
 export default BlogPage;
