0ef0e877df6d9b393600db4f247bbe1915c062a3
Chen Yu Ling <yo0.guitar.it@gmail.com>
2024-06-13 01:12:08 +0800

    feat : Add Dynamic Images from Data

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import Layout from "../components/layout";
-import Seo from "../components/seo";
-import { graphql } from "gatsby";
+import Layout from "../../components/layout";
+import Seo from "../../components/seo";
+import { graphql,Link } from "gatsby";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My blog Posts">
       {data.allMdx.edges.map((edge) => (
         <article key={edge.id}>
-          <h2>{edge.node.frontmatter.title}</h2>
+          <h2>
+            <Link to={`/blog/${edge.node.frontmatter.slug}`} >
+              {edge.node.frontmatter.title}
+            </Link>
+          </h2>
           <p>Posted: {edge.node.frontmatter.date}</p>
           <p>{edge.node.excerpt}</p>
         </article>
@@ -25,6 +29,7 @@ export const query = graphql`
           frontmatter {
             date(formatString: "MMMM D,YYYY")
             title
+            slug
           }
           id
           excerpt
